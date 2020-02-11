b5ed2e30ae82fe09c024a7012251ee5f2460e1bd
Dionicio Diaz <dionicio007@hotmail.com>
2020-02-11 00:52:49 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AnimeListComponent } from './components/anime-list/anime-list.component';
+import { CommentsListComponent } from './components/comments-list/comments-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    AnimeListComponent,
+    CommentsListComponent
   ],
   imports: [
     BrowserModule,
