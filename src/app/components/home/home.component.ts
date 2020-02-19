a3519d5824bd228560e240aea30f8f45f6177d23
Dionicio Diaz <dionicio007@hotmail.com>
2020-02-19 01:22:24 -0400

    add post and user interfaces

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from '@services/post.service';
+import { Post } from '@models/post';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  posts: Post[] = [];
 
-  constructor() { }
+  constructor(private postService: PostService) { }
 
   ngOnInit(): void {
+    this.postService.search('?_expand=user&_page=1&_limit=18').subscribe(response => {
+      this.posts = response;
+    });
   }
 
 }
