7424285e90df43f94e3d365fc45d5b8010082bc9
Dionicio Diaz <dionicio007@hotmail.com>
2020-02-20 16:33:55 -0400

    finish users posts component

--- src/app/components/users/users-posts/users-posts.component.ts
+++ src/app/components/users/users-posts/users-posts.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ActivatedRoute } from '@angular/router';
+
+import { User } from '@models/user';
+import { UserService } from '@services/user.service';
+// import { Subject } from 'rxjs';
+
 @Component({
   selector: 'app-users-posts',
   templateUrl: './users-posts.component.html',
   styleUrls: ['./users-posts.component.scss']
 })
 export class UsersPostsComponent implements OnInit {
-
-  constructor() { }
+  user: User;
+  // private userId = new Subject<string>();
+  constructor(private route: ActivatedRoute, private userService: UserService) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      const userId = params.get('userId');
+      if (userId) {
+        this.searchUser(userId);
+      }
+    });
   }
 
+  searchUser(id: string) {
+    this.userService.getById(id, '?_embed=posts').subscribe(response => {
+      this.user = response;
+    });
+  }
 }
